fe9a38246bf41fb7ecb2f4df6a5cc1dca28a7359
Kostya <kolr3002@gmail.com>
2026-01-13 18:20:42 +0500

    Js-homework

---
+++ main.js
@@ -0,0 +1,21 @@
+let user = {}
+
+for(let i = 1; i <= 10; i++) {
+    let name = prompt('Введите ваше имя')
+    let age = +prompt('Введите ваш возраст')
+
+    user[i] = {
+        name: name,
+        age: +age,
+    }
+}
+
+for(let key in user) {
+    console.log(`Пользователь - ${key}`);
+    console.log(`Ваше имя - ${user[key].name}`);
+    console.log(`Ваш возраст - ${user[key].age}`);
+}
+console.log(user);
+
+
+alert(user[3].name)
